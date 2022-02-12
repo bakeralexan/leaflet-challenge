e1e82d450fe066ccacc8e25ae81b9046579af8e4
bakera <bakera@alumni.usc.edu>
2022-02-11 21:56:04 -0800

    updated functions

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -2,7 +2,7 @@ const earthquakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary
 const platesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/339b0c56563c118307b1f4542703047f5f698fae/GeoJSON/PB2002_plates.json";
 // const platesURL = "PB2002_plates.json";
 
-function createMap(earthquakes) {
+function createMap() {
     // Adding a tile layer (the background map image) to our map:
     // We use the addTo() method to add objects to our map.
     const street= L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -17,15 +17,15 @@ function createMap(earthquakes) {
         Topography: topo
     };
     const overlayMaps = {
-        Earthquakes: earthquakes,
-        Techtonic_Plates: plates
+        Earthquakes: earthquakeMarkers,
+        Techtonic_Plates: plateLine
     };
 
     // Create a map object, and set the default layers.
     const myMap = L.map("map", {
         center: [37.0902, -95.7129],
         zoom: 5,
-        layers: [street, earthquakes]
+        layers: [street, earthquakeMarkers]
     });
 
     // Pass our map layers into our layer control.
@@ -33,29 +33,34 @@ function createMap(earthquakes) {
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap);
-}
-function createEarthquakeMarkers(response) {
-    let features = response.features;
-    let earthquakeMarkers = [];
-    for (let i = 0; i < features.length; i++) {
-        let feature = features[i];
-        let geometry = feature.geometry;
-        let property = feature.properties;
-        let earthquakeMarker = L.circle([geometry.coordinates[0], geometry.coordinates[1]]).bindPopup(`<h1> ${property.place}</h1><hr><h3>Magnitude: ${property.mag}</h3>`);
-        earthquakeMarkers.push(earthquakeMarker);
-    }
-}
-d3.json(earthquakeURL).then(createEarthquakeMarkers);
-function createPlates(response) {
-    let features = response.features;
-    let plateLines = [];
-    for (let i = 0; i < features.length; i++) {
-        let feature = features[i];
-        let geometry = feature.geometry;
-        let property = feature.properties;
-        let line = geometry.coordinates[i]
-        let plateLine = L.polyline([line]).bindPopup(`<h1> ${property.PlateName}</h1>`);
-        plateLines.push(plateLine);
-    }
-}
-d3.json(platesURL).then(createPlates);
+    
+    console.log("Hello");
+    d3.json(earthquakeURL).then(function createEarthquakeMarkers(response) {
+        let features = response.features;
+        let earthquakeMarkers = [];
+        for (let i = 0; i < features.length; i++) {
+            let feature = features[i];
+            let geometry = feature.geometry;
+            let property = feature.properties;
+            let earthquakeMarker = L.circle([geometry.coordinates[0], geometry.coordinates[1]]).bindPopup(`<h1> ${property.place}</h1><hr><h3>Magnitude: ${property.mag}</h3>`);
+            earthquakeMarkers.push(earthquakeMarker);
+        }
+    });
+    createEarthquakeMarkers(earthquakeMarkers);
+    
+    d3.json(platesURL).then(function createPlates(response) {
+        let features = response.features;
+        let plateLines = [];
+        for (let i = 0; i < features.length; i++) {
+            let feature = features[i];
+            let geometry = feature.geometry;
+            let property = feature.properties;
+            let line = geometry.coordinates[i]
+            let plateLine = L.polyline([line]).bindPopup(`<h1> ${property.PlateName}</h1>`);
+            plateLines.push(plateLine);
+        }
+    });
+    createPlates(plateLines);
+};
+createMap();
+
