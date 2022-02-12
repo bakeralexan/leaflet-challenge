8f1acd2b0dd71f6a888f0f49a9686a9a11fc5aed
bakera <bakera@alumni.usc.edu>
2022-02-11 21:34:02 -0800

    updated code

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,26 +1,61 @@
-function createMap()
-    var myMap = L.map("map", {
-        center: [37.7749, -122.4194],
-        zoom: 13
-    });
-    
-    // Adding the tile layer
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+const earthquakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+const platesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/339b0c56563c118307b1f4542703047f5f698fae/GeoJSON/PB2002_plates.json";
+// const platesURL = "PB2002_plates.json";
+
+function createMap(earthquakes) {
+    // Adding a tile layer (the background map image) to our map:
+    // We use the addTo() method to add objects to our map.
+    const street= L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(myMap);
-    
-    var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
-    
-    d3.json(url).then(function(response) {
-    
-        console.log(response);
-    
-        for (var i = 0; i < response.length; i++) {
-        var location = response[i].location;
-    
-        if (location) {
-            L.marker([location.coordinates[1], location.coordinates[0]]).addTo(myMap);
-        }
-        }
-    
     });
+    const topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
+    
+    const baseMaps = {
+        Street: street,
+        Topography: topo
+    };
+    const overlayMaps = {
+        Earthquakes: earthquakes,
+        Techtonic_Plates: plates
+    };
+
+    // Create a map object, and set the default layers.
+    const myMap = L.map("map", {
+        center: [37.0902, -95.7129],
+        zoom: 5,
+        layers: [street, earthquakes]
+    });
+
+    // Pass our map layers into our layer control.
+    // Add the layer control to the map.
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
+}
+function createEarthquakeMarkers(response) {
+    let features = response.features;
+    let earthquakeMarkers = [];
+    for (let i = 0; i < features.length; i++) {
+        let feature = features[i];
+        let geometry = feature.geometry;
+        let property = feature.properties;
+        let earthquakeMarker = L.circle([geometry.coordinates[0], geometry.coordinates[1]]).bindPopup(`<h1> ${property.place}</h1><hr><h3>Magnitude: ${property.mag}</h3>`);
+        earthquakeMarkers.push(earthquakeMarker);
+    }
+}
+d3.json(earthquakeURL).then(createEarthquakeMarkers);
+function createPlates(response) {
+    let features = response.features;
+    let plateLines = [];
+    for (let i = 0; i < features.length; i++) {
+        let feature = features[i];
+        let geometry = feature.geometry;
+        let property = feature.properties;
+        let line = geometry.coordinates[i]
+        let plateLine = L.polyline([line]).bindPopup(`<h1> ${property.PlateName}</h1>`);
+        plateLines.push(plateLine);
+    }
+}
+d3.json(platesURL).then(createPlates);
