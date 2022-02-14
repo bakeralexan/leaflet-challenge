18e7abbdca1ddfe178417822f079dd45be787218
bakera <bakera@alumni.usc.edu>
2022-02-14 00:49:22 -0800

    added colors to markers

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -4,82 +4,71 @@ const platesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/339b0
 
 function createMap() {
     // Adding a tile layer (the background map image) to our map:
-    // We use the addTo() method to add objects to our map.
+
     const street= L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
     const topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
     });
+
     let earthquakeMarkers = L.layerGroup([]);
     d3.json(earthquakeURL).then(function (response) {
         let features = response.features;
-        // put the function outside of the loop then call it in the radius thing
-
-
 
         for (let i = 0; i < features.length; i++) {
             let feature = features[i];
             let geometry = feature.geometry;
             let property = feature.properties;
             let magnitude = property.mag;
-            // gradient: {
-            //     '.5': 'green',
-            //     '.8': 'yellow',
-            //     '.95': 'red'
-            // },
-            // maxOpacity: .9,
-            // minOpacity: .3,
 
             function markerSize(magnitude) {
                 if (magnitude != 0 || magnitude != "NaN") {
-                    return magnitude * 1000;
+                    return magnitude * 10000;
                 } else{
                     magnitude = 0;
                 }
             };
             function colorChange(magnitude) {
-                if (magnitude < 3.50) {
-                    color:"green";
-
-                    return magnitude;
-                } else if (magnitude < 5.50) {
-                    color:"yellow";
-
-                    return magnitude;
-                } else {
-                    color: "red";
-
-                    return magnitude;
-                }
+                if (magnitude < 2.5) return "#2E7F18";
+                else if (magnitude < 5.4) return "#45731E";
+                else if (magnitude < 6.0) return "#675E24";
+                else if (magnitude < 6.9) return "#8D472B";
+                else if (magnitude < 7.9) return "#B13433";
+                else return " #C82538";
             }; 
-            function fillColorChange(magnitude) {
-                if (magnitude < 3.50) {
-
-                    fiilColor: "green";
-                    return magnitude;
-                } else if (magnitude < 5.50) {
-
-                    fillColor: "yellow";
-                    return magnitude;
-                } else {
-
-                    fillColor: "red";
-                    return magnitude;
-                }
-            };
 
             let earthquakeMarker = L.circle([geometry.coordinates[1], geometry.coordinates[0]], {
                 color: colorChange(magnitude),
-                fillColor: fillColorChange(magnitude),
-                fillOpacity: 0.5,
+                fillColor: colorChange(magnitude),
+                fillOpacity: 0.75,
                 radius: markerSize(magnitude)
             }).bindPopup(`<h1> ${property.place}</h1><hr><h3>Magnitude: ${magnitude}</h3>`);
             earthquakeMarker.addTo(earthquakeMarkers);
         }
-        // return earthquakeMarkers;
         earthquakeMarkers.addTo(myMap);
     }) 
+
+    let legend = L.control({ position: "bottomright" });
+    legend.onAdd = function() {
+        let div = L.DomUtil.create("div", "info legend");
+        let limits = earthquakeMarkers.options.limits;
+        let colors = earthquakeMarkers.options.colors;
+        let labels = [];
+        let legendInfo = "<h1>Magnitude</h1>" +
+            "<div class=\"labels\">" +
+            "<div class=\"min\">" + limits[0] + "</div>" +
+            "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+            "</div>";
+        div.innerHTML = legendInfo;
+        limits.forEach(function(limit, index) {
+            labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+        });
+  
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        return div;
+        legend.addTo(myMap);
+    };
 
     let plateLines = L.layerGroup([]);
 
@@ -121,7 +110,7 @@ function createMap() {
     // Add the layer control to the map.
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
-    }).addTo(myMap);
+    }).addTo(myMap); // We use the addTo() method to add objects to our map.
 
 
 }
